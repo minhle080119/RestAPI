73d4cd4a07424a612c08e8495f27e9749fd2be4f
minhle080119 <minhle080119@gmail.com>
2018-10-22 15:36:05 -0500

    Revert "env"
    This reverts commit 676d4afea28b6b2f83cc540ed33e34129da2f1a2.

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\minh\desktop\learning\django\projects\restapi\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
